cb439bacfc38e0f1e54a02e56a8baadad14ad89a
leonkae <leon29kae@gmail.com>
2022-05-27 10:41:56 +0300

    new files added/initial project setup complete.

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    return render(request, 'gallery/gallery.html')
+
+def viewPhoto(request,pk):
+    return render(request,'gallery/photo.html')
+
+def addPhoto(request):
+    return render(request,'gallery/add.html')
