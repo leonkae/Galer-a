f6b63ab1d1cfa07c00c6ca8afd28dca6b7a0b516
leonkae <leon29kae@gmail.com>
2022-05-28 00:09:11 +0300

    modified views and templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from email.mime import image
+from django.shortcuts import render, redirect
 from .models import Category, Location, Image
 # Create your views here.
 
@@ -17,4 +18,28 @@ def viewPhoto(request,pk):
     return render(request,'gallery/photo.html',{'images':images})
 
 def addPhoto(request):
-    return render(request,'gallery/add.html')
+    categories = Category.objects.all()
+    
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] !='':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None 
+        
+        images = Image.objects.create(
+            category=category,
+            description=data['description'],
+            location=data['location'],
+            image=image,
+        )  
+        
+        return redirect('gallery')       
+            
+        
+    
+    context = {'categories': categories}
+    return render(request,'gallery/add.html', context)
