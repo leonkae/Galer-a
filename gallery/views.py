7850fce4c843e3e6f5f98148c3106641866ad519
leonkae <leon29kae@gmail.com>
2022-05-27 21:28:32 +0300

    updated urls and templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from .models import Category, Location, Image
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'gallery/gallery.html')
+    categories =  Category.objects.all()
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    
+    context = {'categories': categories,'images':images,'locations':locations}
+    
+    return render(request, 'gallery/gallery.html',context )
 
 def viewPhoto(request,pk):
-    return render(request,'gallery/photo.html')
+    images = Image.objects.get(id=pk)
+    
+    return render(request,'gallery/photo.html',{'images':images})
 
 def addPhoto(request):
     return render(request,'gallery/add.html')
