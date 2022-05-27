19310373856ba680e70129490a4518f9cd948b74
leonkae <leon29kae@gmail.com>
2022-05-27 14:46:59 +0300

    pillow installed

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    location =  models.ForeignKey(Location, on_delete=models.SET_NULL , null=True)
+    
+    
+    def __str__(self):
+        return self.description
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)    
+    
+    def __str__(self):
+        return self.name
+    
+class Location(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)    
+    
+    def __str__(self):
+        return self.name  
+    
